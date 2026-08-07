c6dd520efb52a740d3a812df0c3bbe4f99f83adb
Michael Carroll <mjoecarroll@gmail.com>
2026-08-07 17:03:37 -0400

    Add const for save file path

--- nardole/const.py
+++ nardole/const.py
@@ -63,6 +63,7 @@ INTEGRATION_DATA_DIR = DATA_DIR.joinpath("integrations")
 CONFIG_PATH = Path("/", "config")
 CONFIG_FILE = CONFIG_PATH.joinpath("config.yaml")
 PERMISSIONS_FILE_PATH = STORE_PATH.joinpath("permissions.json")
+SAVE_FILE_PATH = DATA_DIR.joinpath("files")
 
 INDEX_CONTACTS = "contact"
 INDEX_EMAIL_ADDRESSES = "email_addresses"
